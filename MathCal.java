9d32a48b37363221bcf99fa19590c0546ac5f8dd
sys_smlz <sys_smlz@qq.com>
2020-07-27 21:10:17 +0800

    首次提交angel-eyes代码

---
+++ MathCal.java
@@ -0,0 +1,16 @@
+package com.tuling.math;
+
+/**
+ * [来个全套]
+ *
+ * @slogan: 高于生活，源于生活
+ * @Description: TODO
+ * @author: smlz
+ * @date 2020/7/27 21:08
+ */
+public class MathCal {
+
+    public Integer add(int numA,int numB) {
+        return numA+numB;
+    }
+}
